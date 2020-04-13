f2b3da8005d93d66f9f3f91395a82a7a9ae2302d
jolson615 <jolson615@gmail.com>
2020-04-13 17:27:17 -0400

    imports Flask

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import render_template, request
+# ---- YOUR APP STARTS HERE ----
+# -- Import section --
+from flask import Flask, render_template, request
 from model import shout
 
+
+# -- Initialization section --
+app = Flask(__name__)
+
+
+# -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
